8a9d3645e205c10b69bcb797590a9ca215e8671e
PriyeshPandey2000 <priyeshpandey2000@gmail.com>
2024-02-27 12:49:03 +0530

    .

--- server/config/db.js
+++ server/config/db.js
@@ -8,7 +8,9 @@ mongoose.connect(MONGODB_URI);
 
 const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on('error', (error) => {
+  console.error('MongoDB connection error:', error);
+});
 db.once('open', () => {
   console.log('Connected to MongoDB');
 });
